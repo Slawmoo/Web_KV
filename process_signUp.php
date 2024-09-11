060fd93b384f81f71176734ea9e7924a14a4e520
Marko <karavla.marko@gmail.com>
2024-09-11 10:50:29 +0200

    Validacija radi, Ali treba doradit php funkciju validacije

--- process_signUp.php
+++ process_signUp.php
@@ -1,8 +1,8 @@
 <?php
 // Spajanje na bazu podataka
-$servername = "localhost"; // Ili IP adresa servera
-$username = "root"; // Korisničko ime baze
-$password = ""; // Lozinka baze (ostavi prazno ako nema lozinke)
+$servername = "localhost";
+$username = "root";
+$password = "";
 $dbname = "cv_data";
 
 // Kreiraj konekciju
@@ -16,20 +16,34 @@ if ($conn->connect_error) {
 // Uhvati podatke iz POST zahtjeva
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $conn->real_escape_string($_POST['email']);
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hash lozinke radi sigurnosti
+    $password = $_POST['password'];
     $description = $conn->real_escape_string($_POST['description']);
     $company = $conn->real_escape_string($_POST['company']);
 
-    // Unos podataka u bazu
-    $sql = "INSERT INTO users (email, password, description, company, isAdmin) VALUES ('$email', '$password', '$description', '$company', 0)";
+    // Validacija lozinke na serveru
+    if (!preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/", $password)) {
+        die("Password does not meet the required complexity.");
+    }
+
+    // Provjera je li e-mail već registriran
+    $checkEmail = $conn->query("SELECT * FROM users WHERE email = '$email'");
+    if ($checkEmail->num_rows > 0) {
+        die("Email is already registered.");
+    }
+
+    // Ako je e-mail jedinstven i lozinka valjana, hashiraj lozinku i unesi podatke
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+    $sql = "INSERT INTO users (email, password, description, company, isAdmin) VALUES ('$email', '$hashedPassword', '$description', '$company', 0)";
 
     if ($conn->query($sql) === TRUE) {
-        // Uspješno izvršeno - redirekcija s parametrom
         header("Location: signIn.html?status=success");
+        exit(); 
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
+
 // Zatvori konekciju
 $conn->close();
 ?>
