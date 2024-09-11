61b3f7145da29de1107f6e7d0ff4603809dc0dfb
Marko <karavla.marko@gmail.com>
2024-09-11 15:48:04 +0200

    Odvojen sidebar.php u centralnu datoteku i započet CRUD nad data

--- home.php
+++ home.php
@@ -15,41 +15,12 @@ $user_name = isset($_SESSION['user_name']) ? $_SESSION['user_name'] : 'Guest'; /
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <title>HOME</title>
     <style>/* Stil za desno poravnat tekst u zaglavlju */
-        #welcome-text {
-        float: right;
-        font-size: 23px;
-        color: #FFE500;
-        margin-right: 20px;
-    }   
+        
     </style>
 </head>
 
 <body>
-    <div id="sidebar">
-        <a href="home.php">Home</a>
-        <a href="sendMsg.php">Send Message</a>
-        <a href="signUp.php">Sign Up</a>
-        <a href="signIn.php">Sign In</a>
-        <a href="signOut.php">Sign Out</a>
-        <a href="FAQ.php">FAQ</a>
-        <a href="#" onclick="closeNav()">Close Menu</a>
-
-        <!-- User Info Section -->
-        <div id="userInfo">
-            <div>ACCOUNT</div><br>
-            <?php if (isset($_SESSION['user_name']) && isset($_SESSION['userEmail']) && isset($_SESSION['userCompany']) && isset($_SESSION['userDescription'])): ?>
-                <div id="user_name"><?php echo htmlspecialchars($_SESSION['user_name']); ?></div>
-                <div id="userEmail"><?php echo htmlspecialchars($_SESSION['userEmail']); ?></div>
-                <div id="userCompany"><?php echo htmlspecialchars($_SESSION['userCompany']); ?></div>
-                <div id="userDescription" onclick="toggleDescription()">
-                    <span id="shortDescription"><?php echo htmlspecialchars(substr($_SESSION['userDescription'], 0, 20)) . (strlen($_SESSION['userDescription']) > 20 ? '...' : ''); ?></span>
-                    <span id="fullDescription"><?php echo htmlspecialchars($_SESSION['userDescription']); ?></span>
-                </div>
-            <?php else: ?>
-                <div>Sign in to see information</div>
-            <?php endif; ?>
-        </div>
-    </div>
+    <?php include 'sidebar.php'; ?>
 
     <header>
         <h1 id="mainTitle">Home and M. K.</h1>
---
+++ sidebar.php
@@ -0,0 +1,28 @@
+<!-- sidebar.php -->
+<div id="sidebar">
+    <a href="home.php">Home</a>
+    <a href="sendMsg.php">Send Message</a>
+    <a href="signUp.php">Sign Up</a>
+    <a href="signIn.php">Sign In</a>
+    <a href="signOut.php">Sign Out</a>
+    <a href="FAQ.php">FAQ</a>
+    <a href="#" onclick="closeNav()">Close Menu</a>
+
+    <!-- User Info Section -->
+    <div id="userInfo">
+        <div>ACCOUNT</div><br>
+        <?php if (isset($_SESSION['user_name']) && isset($_SESSION['userEmail']) && isset($_SESSION['userCompany']) && isset($_SESSION['userDescription'])): ?>
+            <div id="user_name"><?php echo htmlspecialchars($_SESSION['user_name']); ?></div>
+            <div id="userEmail"><?php echo htmlspecialchars($_SESSION['userEmail']); ?></div>
+            <div id="userCompany"><?php echo htmlspecialchars($_SESSION['userCompany']); ?></div>
+            <div id="userDescription" onclick="toggleDescription()">
+                <span id="shortDescription"><?php echo htmlspecialchars(substr($_SESSION['userDescription'], 0, 20)) . (strlen($_SESSION['userDescription']) > 20 ? '...' : ''); ?></span>
+                <span id="fullDescription"><?php echo htmlspecialchars($_SESSION['userDescription']); ?></span>
+            </div>
+            <!-- Edit Button -->
+            <button id="editUserInfoButton">Edit Info</button>
+        <?php else: ?>
+            <div>Sign in to see information</div>
+        <?php endif; ?>
+    </div>
+</div>
